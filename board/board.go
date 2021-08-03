8b11d24a5b06ec069e52cb97b3b4fe7c30b99bee
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2021-08-02 22:39:09 -0400

    Added in a shader compile helper function and got a point drawn

--- board/board.go
+++ board/board.go
@@ -4,7 +4,63 @@ import (
 	"syscall/js"
 
 	"github.com/nicholasblaskey/webgl/webgl"
+
+	"errors"
 )
+
+func CreateProgram(gl *webgl.Gl, vertexSource, fragmentSource string) (*webgl.Program, error) {
+	vShader, err := createShader(gl, webgl.VERTEX_SHADER, vertexSource)
+	if err != nil {
+		return nil, err
+	}
+	fShader, err := createShader(gl, webgl.FRAGMENT_SHADER, fragmentSource)
+	if err != nil {
+		return nil, err
+	}
+
+	program := gl.CreateProgram()
+	if program == nil {
+		return nil, errors.New("Could not create program")
+	}
+
+	gl.AttachShader(program, vShader)
+	gl.AttachShader(program, fShader)
+	gl.LinkProgram(program)
+
+	linked := gl.GetProgramParameter(program, webgl.LINK_STATUS)
+	if linked == 0 {
+		info := gl.GetProgramInfoLog(program)
+
+		gl.DeleteProgram(program)
+		gl.DeleteShader(vShader)
+		gl.DeleteShader(fShader)
+		return nil, errors.New(info)
+	}
+
+	gl.UseProgram(program)
+
+	return program, nil
+}
+
+func createShader(gl *webgl.Gl, shaderType int, source string) (*webgl.Shader, error) {
+	shader := gl.CreateShader(shaderType)
+	if shader == nil {
+		return nil, errors.New("Could not create shader")
+	}
+
+	gl.ShaderSource(shader, source)
+	gl.CompileShader(shader)
+
+	compiled := gl.GetShaderParameter(shader, webgl.COMPILE_STATUS)
+	if compiled == 0 {
+		info := gl.GetShaderInfoLog(shader)
+
+		gl.DeleteShader(shader)
+		return nil, errors.New(info)
+	}
+
+	return shader, nil
+}
 
 func main() {
 	document := js.Global().Get("document")
@@ -17,23 +73,23 @@ func main() {
 	gl.ClearColor(0.3, 0.5, 0.3, 1.0)
 	gl.Clear(webgl.COLOR_BUFFER_BIT)
 
-	/*
-			vertShader := `
+	vertShader := `
 		void main() {
 			gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
 			gl_PointSize = 10.0;
 		}`
-			fragShader := `
+	fragShader := `
 		void main() {
 			gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 		}`
-			if !util.InitShaders(gl, vertShader, fragShader) {
-				fmt.Println("Failed to init shaders")
-			}
-	*/
+	program, err := CreateProgram(gl, vertShader, fragShader)
+	if err != nil {
+		panic(err)
+	}
+	_ = program
 
 	//gl.Call("clearColor", 0.0, 0.0, 0.0, 1.0)
 	//gl.Call("clear", gl.Get("COLOR_BUFFER_BIT"))
-	//gl.Call("drawArrays", gl.Get("POINTS"), 0, 1)
+	gl.DrawArrays(webgl.POINTS, 0, 1)
 
 }
