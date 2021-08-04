9fa1c51650e7a3e8394449e5a32caf5b1b25b036
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2021-08-03 20:07:16 -0400

    Moved shader functionality to utils

--- board/board.go
+++ board/board.go
@@ -3,64 +3,10 @@ package main
 import (
 	"syscall/js"
 
+	"github.com/nicholasblaskey/webgl-utils/util"
+
 	"github.com/nicholasblaskey/webgl/webgl"
-
-	"errors"
 )
-
-func CreateProgram(gl *webgl.Gl, vertexSource, fragmentSource string) (*webgl.Program, error) {
-	vShader, err := createShader(gl, webgl.VERTEX_SHADER, vertexSource)
-	if err != nil {
-		return nil, err
-	}
-	fShader, err := createShader(gl, webgl.FRAGMENT_SHADER, fragmentSource)
-	if err != nil {
-		return nil, err
-	}
-
-	program := gl.CreateProgram()
-	if program == nil {
-		return nil, errors.New("Could not create program")
-	}
-
-	gl.AttachShader(program, vShader)
-	gl.AttachShader(program, fShader)
-	gl.LinkProgram(program)
-
-	linked := gl.GetProgramParameter(program, webgl.LINK_STATUS)
-	if linked == 0 {
-		info := gl.GetProgramInfoLog(program)
-
-		gl.DeleteProgram(program)
-		gl.DeleteShader(vShader)
-		gl.DeleteShader(fShader)
-		return nil, errors.New(info)
-	}
-
-	gl.UseProgram(program)
-
-	return program, nil
-}
-
-func createShader(gl *webgl.Gl, shaderType int, source string) (*webgl.Shader, error) {
-	shader := gl.CreateShader(shaderType)
-	if shader == nil {
-		return nil, errors.New("Could not create shader")
-	}
-
-	gl.ShaderSource(shader, source)
-	gl.CompileShader(shader)
-
-	compiled := gl.GetShaderParameter(shader, webgl.COMPILE_STATUS)
-	if compiled == 0 {
-		info := gl.GetShaderInfoLog(shader)
-
-		gl.DeleteShader(shader)
-		return nil, errors.New(info)
-	}
-
-	return shader, nil
-}
 
 func main() {
 	document := js.Global().Get("document")
@@ -82,7 +28,7 @@ func main() {
 		void main() {
 			gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 		}`
-	program, err := CreateProgram(gl, vertShader, fragShader)
+	program, err := util.CreateProgram(gl, vertShader, fragShader)
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -2,6 +2,11 @@ module github.com/nicholasblaskey/svg-rasterizer
 
 go 1.14
 
-require github.com/nicholasblaskey/webgl v0.0.0 // indirect
+require (
+	github.com/nicholasblaskey/webgl v0.0.0 // indirect
+	github.com/nicholasblaskey/webgl-utils v0.0.0 // indirect
+)
 
 replace github.com/nicholasblaskey/webgl v0.0.0 => ../webgl
+
+replace github.com/nicholasblaskey/webgl-utils v0.0.0 => ../webgl-utils
