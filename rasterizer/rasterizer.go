ff6de7364a96dcd3628cfcc8991cbd25d1706865
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2021-08-23 20:53:47 -0400

    Finished implmeenting the algorithm unsucessfully

--- rasterizer/rasterizer.go
+++ rasterizer/rasterizer.go
@@ -22,7 +22,10 @@ type vec2 struct {
 	y float32
 }
 
-func triangulate(points []float32) []Triangle {
+// Code translated from
+// https://github.com/CMU-Graphics/DrawSVG/blob/master/src/triangulation.cpp
+// TODO find and implement an algorithm on your own.
+func triangulate(points []float32) []*Triangle {
 	contour := []vec2{}
 	for i := 0; i < len(points); i += 2 {
 		contour = append(contour, vec2{points[i], points[i+1]})
@@ -49,7 +52,7 @@ func triangulate(points []float32) []Triangle {
 	nv := n
 
 	// Remove nv-2 Vertices, each time creating a triangle
-	triangles := []Triangle{}
+	triangles := []*Triangle{}
 	count := 2 * nv // Error detection
 	for m, v := 0, nv-1; nv > 2; {
 		// If we loop it is likely a non-simple polygon
@@ -76,14 +79,87 @@ func triangulate(points []float32) []Triangle {
 			var a, b, c, s, t int
 			a, b, c = V[u], V[v], V[w]
 
-			triangles = append(triangles, Triangle{
+			triangles = append(triangles, &Triangle{
 				contour[a].x, contour[a].y,
 				contour[b].x, contour[b].y,
 				contour[c].x, contour[c].y,
 			})
+
+			m += 1
+
+			// Remove v from remaining polygon
+			s, t = v, v+1
+			for t < nv {
+				V[s] = V[t]
+				s += 1
+				t += 1
+				nv -= 1
+			}
+
+			count = 2 * nv // reset error detection counter
 		}
 	}
 	return triangles
+}
+
+func area(contour []vec2) float32 {
+	n := len(contour)
+	a := float32(0.0)
+
+	p, q := n-1, 0
+	for q < n {
+		a += contour[p].x*contour[q].y - contour[q].x*contour[p].y
+
+		q += 1
+		p = q
+	}
+	return a * 0.5
+}
+
+func snip(contour []vec2, u, v, w, n int, V []int) bool {
+	const EPSILON = 0.0000000001
+
+	Ax := contour[V[u]].x
+	Ay := contour[V[u]].y
+
+	Bx := contour[V[v]].x
+	By := contour[V[v]].y
+
+	Cx := contour[V[w]].x
+	Cy := contour[V[w]].y
+
+	if EPSILON > (((Bx - Ax) * (Cy - Ay)) - ((By - Ay) * (Cx - Ax))) {
+		return false
+	}
+
+	for p := 0; p < n; p++ {
+		if p == u || p == v || p == w {
+			continue
+		}
+		Px := contour[V[p]].x
+		Py := contour[V[p]].y
+		if inside(Ax, Ay, Bx, By, Cx, Cy, Px, Py) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func inside(Ax, Ay, Bx, By, Cx, Cy, Px, Py float32) bool {
+	ax, ay := Cx-Bx, Cy-By
+	bx, by := Ax-Cx, Ay-Cy
+	cx, cy := Bx-Ax, By-Ay
+
+	apx, apy := Px-Ax, Py-Ay
+	bpx, bpy := Px-Bx, Py-By
+	cpx, cpy := Px-Cx, Py-Cy
+
+	aCROSSbp := ax*bpy - ay*bpx
+	cCROSSap := cx*apy - cy*apx
+	bCROSScp := bx*cpy - by*cpx
+
+	return aCROSSbp >= 0.0 && bCROSScp >= 0.0 && cCROSSap >= 0.0
 }
 
 func maxOfThree(x, y, z float32) float32 {
@@ -228,7 +304,7 @@ type Triangle struct {
 	Y3 float32
 }
 
-func pointsToTriangles(in string) []Triangle {
+func pointsToTriangles(in string) []*Triangle {
 	// TODO implmenet an algorithm to convert polygons into triangles
 	// https://stackoverflow.com/questions/7316000/convert-polygon-to-triangles
 	points := strings.Split(strings.Trim(in, " "), " ")
@@ -241,26 +317,33 @@ func pointsToTriangles(in string) []Triangle {
 		pointsFloat = append(pointsFloat, float32(x), float32(y))
 	}
 
-	t := Triangle{
-		pointsFloat[0],
-		pointsFloat[1],
-		pointsFloat[2],
-		pointsFloat[3],
-		pointsFloat[4],
-		pointsFloat[5],
-	}
-	// Sort triangle such that y1 < y2 < y3
-	if t.Y1 > t.Y3 {
-		t.X1, t.Y1, t.X3, t.Y3 = t.X3, t.Y3, t.X1, t.Y1
-	}
-	if t.Y1 > t.Y2 {
-		t.X1, t.Y1, t.X2, t.Y2 = t.X2, t.Y2, t.X1, t.Y1
-	}
-	if t.Y2 > t.Y3 {
-		t.X2, t.Y2, t.X3, t.Y3 = t.X3, t.Y3, t.X2, t.Y2
-	}
+	triangles := triangulate(pointsFloat)
+	for _, t := range triangles {
 
-	return []Triangle{t}
+		//}
+		//for _, t
+		/*
+			t := Triangle{
+				pointsFloat[0],
+				pointsFloat[1],
+				pointsFloat[2],
+				pointsFloat[3],
+				pointsFloat[4],
+				pointsFloat[5],
+			}
+		*/
+		// Sort triangle such that y1 < y2 < y3
+		if t.Y1 > t.Y3 {
+			t.X1, t.Y1, t.X3, t.Y3 = t.X3, t.Y3, t.X1, t.Y1
+		}
+		if t.Y1 > t.Y2 {
+			t.X1, t.Y1, t.X2, t.Y2 = t.X2, t.Y2, t.X1, t.Y1
+		}
+		if t.Y2 > t.Y3 {
+			t.X2, t.Y2, t.X3, t.Y3 = t.X3, t.Y3, t.X2, t.Y2
+		}
+	}
+	return triangles //[]Triangle{t}
 }
 
 func (s *Polygon) rasterize(r *rasterizer) {
@@ -451,7 +534,8 @@ func main() {
 
 	//r, err := New(canvas, "/svg/test1.svg")
 	//r, err := New(canvas, "/svg/test2.svg")
-	r, err := New(canvas, "/svg/test4.svg")
+	//r, err := New(canvas, "/svg/test4.svg")
+	r, err := New(canvas, "/svg/test5.svg")
 	if err != nil {
 		panic(err)
 	}
