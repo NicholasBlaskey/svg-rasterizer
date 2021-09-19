b3d63e94116859f443f33fab5cce22d94ccc10b9
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2021-09-19 18:10:21 -0400

    Moved dat gui js stuff to an external repo

--- go.mod
+++ go.mod
@@ -1,13 +1,18 @@
 module github.com/nicholasblaskey/svg-rasterizer
 
-go 1.14
+go 1.17
 
 require (
 	github.com/go-gl/mathgl v1.0.0
+	github.com/nicholasblaskey/dat-gui-go-wasm v0.0.0-20210919220437-82602fcef295
 	github.com/nicholasblaskey/webgl v0.0.0
 	github.com/nicholasblaskey/webgl-utils v0.0.0
 )
 
+require golang.org/x/image v0.0.0-20190321063152-3fc05d484e9f // indirect
+
 replace github.com/nicholasblaskey/webgl v0.0.0 => ../webgl
 
 replace github.com/nicholasblaskey/webgl-utils v0.0.0 => ../webgl-utils
+
+replace github.com/nicholasblaskey/dat-gui-go-wasm v0.0.0 => ../dat-gui-go-wasm
--- rasterizer/rasterizer.go
+++ rasterizer/rasterizer.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/xml"
 	"fmt"
+	"image"
+	"image/png"
 	"io/ioutil"
 	"math"
 	"math/rand"
@@ -12,14 +15,8 @@ import (
 	"strings"
 	"syscall/js"
 
-	"reflect"
-
-	"encoding/base64"
-	"image"
-	//	"image/color"
-	"image/png"
-
 	mgl "github.com/go-gl/mathgl/mgl32"
+	"github.com/nicholasblaskey/dat-gui-go-wasm/datGUI"
 
 	"github.com/nicholasblaskey/svg-rasterizer/board"
 	"github.com/nicholasblaskey/svg-rasterizer/triangulate"
@@ -916,139 +913,9 @@ type testType struct {
 	Fun func()
 }
 
-type GUI struct {
-	JSGUI js.Value
-}
-
-type controller struct {
-	JSController js.Value
-	changeFunc   func(js.Value)
-	listenerFunc func()
-}
-
-func (c *controller) Min(x int) *controller {
-	c.JSController = c.JSController.Call("min", x)
-	c.changeFunc(c.JSController)
-	return c
-}
-
-func (c *controller) Max(x int) *controller {
-	c.JSController = c.JSController.Call("max", x)
-	c.changeFunc(c.JSController)
-	return c
-}
-
-func (c *controller) Step(x int) *controller {
-	c.JSController = c.JSController.Call("max", x)
-	c.changeFunc(c.JSController)
-	return c
-}
-
-func (c *controller) Name(x string) *controller {
-	c.JSController = c.JSController.Call("name", x)
-	c.changeFunc(c.JSController)
-	return c
-}
-
-func (c *controller) OnChange(fun func()) *controller {
-	c.listenerFunc = fun
-
-	return c
-}
-
-func GUINew() *GUI {
-	return &GUI{JSGUI: js.Global().Get("dat").Get("GUI").New()}
-}
-
-func (g *GUI) Add(obj interface{}, fieldName string) *controller {
-	// Get our field value.
-	structVal := reflect.Indirect(reflect.ValueOf(obj))
-	fieldVal := structVal.FieldByName(fieldName)
-	fieldType := fieldVal.Type()
-
-	// Make our field into a javascript object.
-	jsObjMap := make(map[string]interface{})
-	switch fieldType.Name() {
-	case "float32":
-		jsObjMap[fieldName] = fieldVal.Float()
-	case "float64":
-		jsObjMap[fieldName] = fieldVal.Float()
-	case "int":
-		jsObjMap[fieldName] = fieldVal.Int()
-	case "int64":
-		jsObjMap[fieldName] = fieldVal.Int()
-	case "bool":
-		jsObjMap[fieldName] = fieldVal.Bool()
-	case "string":
-		jsObjMap[fieldName] = fieldVal.String()
-	case "": // Func type.
-		jsObjMap[fieldName] = js.FuncOf(
-			func(this js.Value, args []js.Value) interface{} {
-				fieldVal.Call(nil)
-				return nil
-			})
-	default:
-		panic("Tried to set unsupported type of " +
-			fieldType.Name() + " in object ")
-	}
-
-	jsObj := js.ValueOf(jsObjMap)
-
-	// We need this changeFunction since we will need to set the value again with
-	// the new javascript controller when calls happen of both .min and .max for
-	// some reason.
-	c := &controller{}
-	c.JSController = g.JSGUI.Call("add", jsObj, fieldName)
-	c.changeFunc = func(jsController js.Value) {
-		jsController.Call("onChange", js.FuncOf(
-			func(this js.Value, args []js.Value) interface{} {
-				// Set the value from the Javascript object to our Go object.
-				changed := jsObj.Get(fieldName)
-				f := reflect.ValueOf(obj).Elem().FieldByName(fieldName)
-
-				switch fieldType.Name() {
-				case "float32":
-					f.SetFloat(changed.Float())
-				case "float64":
-					f.SetFloat(changed.Float())
-				case "int":
-					f.SetInt(int64(changed.Int()))
-				case "bool":
-					f.SetBool(changed.Bool())
-				case "string":
-					f.SetString(changed.String())
-				case "": // Func type. Do nothing since we don't need to assign anything back.
-				default:
-					panic("We should never reach this since we check the type above.")
-				}
-
-				if c.listenerFunc != nil {
-					c.listenerFunc()
-				}
-				return nil
-			}))
-	}
-	c.changeFunc(c.JSController)
-
-	return c
-}
-
-func (g *GUI) AddFolder(name string) *GUI {
-	subJSGUI := g.JSGUI.Call("addFolder", name)
-	return &GUI{JSGUI: subJSGUI}
-}
-
-func (g *GUI) Open() {
-	g.JSGUI.Call("open")
-}
-
-func (g *GUI) Close() {
-	g.JSGUI.Call("close")
-}
-
 func createGui() {
 	// New GUI
-	gui := GUINew()
+	gui := datGUI.New()
 
 	obj := testType{1, true, 3, "String",
 		func() {
@@ -1065,27 +932,17 @@ func createGui() {
 	gui.Add(&obj, "W").Name("This value is a string")
 	gui.Add(&obj, "Fun").Name("alert")
 
-	{
-		obj2 := testType{X: 5}
-		folder := gui.AddFolder("folder")
-		folder.Open()
-		folder.Add(&obj2, "X").Name("X in a folder")
-
-		// select options TODO
-		//
-
-		/* https://github.com/PavelDoGreat/WebGL-Fluid-Simulation/blob/master/script.js
-		   let github = gui.add({ fun : () => {
-		       window.open('https://github.com/PavelDoGreat/WebGL-Fluid-Simulation');
-		       ga('send', 'event', 'link button', 'github');
-		   } }, 'fun').name('Github');
-		   github.__li.className = 'cr function bigFont';
-		   github.__li.style.borderLeft = '3px solid #8C8C8C';
-		   let githubIcon = document.createElement('span');
-		   github.domElement.parentElement.appendChild(githubIcon);
-		   githubIcon.className = 'icon github';
-		*/
-	}
+	/* https://github.com/PavelDoGreat/WebGL-Fluid-Simulation/blob/master/script.js
+	   let github = gui.add({ fun : () => {
+	       window.open('https://github.com/PavelDoGreat/WebGL-Fluid-Simulation');
+	       ga('send', 'event', 'link button', 'github');
+	   } }, 'fun').name('Github');
+	   github.__li.className = 'cr function bigFont';
+	   github.__li.style.borderLeft = '3px solid #8C8C8C';
+	   let githubIcon = document.createElement('span');
+	   github.domElement.parentElement.appendChild(githubIcon);
+	   githubIcon.className = 'icon github';
+	*/
 
 }
 
